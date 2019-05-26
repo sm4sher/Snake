e28c2b605355ffb14da30a2aad0f191def847069
Adrien <sm4sher25@gmail.com>
2019-05-26 22:39:14 +0200

    Initial commit

---
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.mygdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.myGDX.snake.SnakeGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1024;
+		config.height = 1024;
+		new LwjglApplication(new SnakeGame(), config);
+	}
+}
